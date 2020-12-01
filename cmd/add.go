e60f6b46bf6c1b546a6fb867e875378680d84de5
steffakasid <github@steffen-rumpf.de>
2020-12-01 12:20:18 +0100

    Add files to dot file repository

--- cmd/add.go
+++ cmd/add.go
@@ -16,14 +16,13 @@ limitations under the License.
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
 // addCmd represents the add command
 var addCmd = &cobra.Command{
 	Use:   "add",
+	Args:  cobra.ExactArgs(1),
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -32,20 +31,20 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("add called")
+		// config add .vimrc
+		gitAdd := *gitAlias
+		gitAddArgs := []string{"add", args[0]}
+		gitAdd.Args = append(gitAdd.Args, gitAddArgs...)
+		execCmdAndPrint(&gitAdd)
+
+		// config commit -m "Add vimrc"
+		gitCommit := *gitAlias
+		gitCommitArgs := []string{"commit", "-m", "Add " + args[0]}
+		gitCommit.Args = append(gitCommit.Args, gitCommitArgs...)
+		execCmdAndPrint(&gitCommit)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(addCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// addCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
