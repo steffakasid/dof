20f8fa85a17cf74497ab6ec76c54647ccfa9e97b
steffakasid <github@steffen-rumpf.de>
2020-12-01 21:57:21 +0100

    Added sync to push/pull changes

--- cmd/sync.go
+++ cmd/sync.go
@@ -16,14 +16,18 @@ limitations under the License.
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
+)
+
+var (
+	dontPush bool
+	dontPull bool
 )
 
 // syncCmd represents the sync command
 var syncCmd = &cobra.Command{
 	Use:   "sync",
+	Args:  cobra.NoArgs,
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -32,20 +36,21 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("sync called")
+		if !dontPush {
+			pushCmd := *gitAlias
+			pushCmd.Args = append(pushCmd.Args, "push", "origin", branch, "-u")
+			execCmdAndPrint(&pushCmd)
+		}
+		if !dontPull {
+			pullCmd := *gitAlias
+			pullCmd.Args = append(pullCmd.Args, "pull", "--rebase")
+			execCmdAndPrint(&pullCmd)
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(syncCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// syncCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// syncCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	syncCmd.Flags().BoolVarP(&dontPull, "push-only", "P", false, "Only push changes to remote repository.")
+	syncCmd.Flags().BoolVarP(&dontPush, "pull-only", "p", false, "Only pull changes from remote repository.")
 }
